f2e99b4ce5295072b387d348649ab192becf3d9e
Ken Butler <ken@swiss-soccer.net>
2016-04-09 19:17:07 -0400

    First commit.

---
+++ R/calcstats.R
@@ -0,0 +1,51 @@
+#' Calculate an EDF statistic
+#'
+#' Calculate test statistic value (and P-value?) for testing fit to specified distribution.
+#'
+#' @param statfun Name of one of the EDF statistics (see Details)
+#' @param x Vector of data to be tested for fit to the distribution
+#' @param dist Cumulative distribution function of distribution to be fitted
+#' @param ... parameters to distribution
+#'
+#' @details Allowable test statistics are \code{d} (Kolmogorov D), \code{v} (Kuiper V), \code{w2} (Cramer-von Mises W-squared),
+#'   \code{a2} (Anderson-Darling A-squared), \code{u2} (Watson U-squared). Statistics \code{v} and \code{u2} are invariant to choice
+#'   of origin, so can be used for data on a circle (for example, for testing uniformity of times of day).
+#'
+#' @examples
+#' data(leghorn)
+#' # test the transformed data for uniformity (default)
+#' calc.stat(a2,leghorn$z1)
+#' # test original data for normality with mean 200 and SD 35
+#' calc.stat("a2",leghorn$x,pnorm,200,35)
+#' # obtain all statistics
+#' my.stats=c("d","v","w2","a2","u2")
+#' sapply(my.stats,calc.stat,leghorn$x,pnorm,200,35)
+#' @export
+
+calc.stat=function(statfun,x,dist=punif,...) {
+  # dist needs to be a p cdf, with parameters on the end
+  f=match.fun(statfun)
+  z=sort(dist(x,...))
+  f(z)
+}
+
+# simulation
+
+#' Simulate EDF statistics under Case 0
+#'
+#' Obtain simulations of test statistic under assumption that parameters known (for getting P-values).
+#' Case 3 (all parameters unknown) coming up later. Add examples.
+#'
+#' @param n Sample size for each simulated test statistic
+#' @param statfun Name of statistic to be simulated (as in help for \code{calc.stat})
+#' @param nsim Number of simulations to run (default 10000)
+#' @param dist Name of R function to draw random samples from distribution (eg. \code{rnorm})
+#' @param ... parameters for distribution
+
+sim.stat=function(n,statfun,nsim=10000,dist,...) {
+  # dist this time has r on front
+  # how to handle case 3? Need to estimate parameters from each simulated data set
+  # have to handle it case by case, I think
+  z=sort(dist(n,...))
+  calc.stat(statfun,z)
+}
---
+++ R/edfstats.R
@@ -0,0 +1,82 @@
+# worker functions that compute each of the statistics as described in D'Agostino & Stephens (1986) p. 101
+
+#' @export
+
+dplus=function(x) {
+  n=length(x)
+  v=1:n/n-sort(x)
+  max(v)
+}
+
+#' @export
+
+dminus=function(x) {
+  n=length(x)
+  v=sort(x)-(0:(n-1))/n
+  max(v)
+}
+
+#' @export
+
+d=function(x) {
+  max(dplus(x),dminus(x))
+}
+
+#' @export
+
+v=function(x) {
+  dplus(x)+dminus(x)
+}
+
+#' @export
+
+w2=function(x) {
+  n=length(x)
+  I=1:n
+  v=(2*I-1)/2/n
+  w=(x-v)^2
+  sum(w)+1/12/n
+}
+
+#' @export
+
+
+u2=function(x) {
+  n=length(x)
+  zbar=mean(x)
+  w2(x)-n*(zbar-0.5)^2
+}
+
+#' Anderson-Darling A-squared
+#'
+#' @param x sorted, transformed data
+#' @export
+
+a2=function(x) {
+  n=length(x)
+  I=1:n
+  v=2*I-1
+  w=2*n+1-2*I
+  total=v*log(x)+w*log(1-x)
+  -n-sum(total)/n
+}
+
+dmod=function(x){
+  n=length(x)
+  d(x)*(sqrt(n)+0.12+0.11/sqrt(n))
+}
+
+vmod=function(x) {
+  n=length(x)
+  v(x)*(sqrt(n)+0.155+0.24/sqrt(n))
+}
+
+w2mod=function(x) {
+  n=length(x)
+  (w2(x)-0.4/n+0.6/n^2)/(1+1/n)
+}
+
+u2mod=function(x) {
+  n=length(x)
+  (u2(x)-0.1/n+0.1/n^2)/(1+0.8/n)
+}
