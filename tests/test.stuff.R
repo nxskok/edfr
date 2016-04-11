040cd36af0f682d32ede92b0ccad23c5ef6f1020
nxskok <ken@swiss-soccer.net>
2016-04-11 12:12:09 -0400

    making sure test files in right places

--- tests/test.stuff.R
+++
@@ -1,9 +0,0 @@
-library(testthat)
-library(edfr)
-
-test_check("edfr")
-
-test_that("some stuff", {
-  expect_equal(10,10)
-  expect_equal(10,10.01)
-})
--- tests/testthat.R
+++ tests/testthat.R
@@ -2,8 +2,3 @@ library(testthat)
 library(edfr)
 
 test_check("edfr")
-
-test_that("some stuff", {
-  expect_equal(10,10)
-  expect_equal(10,10.01)
-})
